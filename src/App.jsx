39bfa59a0f008ddd5614c3087624cda1148b4737
minseo <kmssally05@gmail.com>
2025-05-15 21:14:37 +0900

    🎉 Vite 기반 프로젝트 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>초기 세팅</div>
+    </>
+  )
+}
+
+export default App
