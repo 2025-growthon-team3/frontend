a9274017359c022922283e8bf47574a464eb79c6
alwn8918 <alwn8918@gmail.com>
2025-05-23 22:29:33 +0900

    Feat: 지도 페이지 기본 구현 (#12)

--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import TabBar from "../../components/TabBar/TabBar";
+import * as L from "./LocationPage.style";
+
+// image
+import myPin from "../../assets/images/songilPinIcon.png";
+import ongiPin from "../../assets/images/ongiPinIcon.png";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const defaultCenter = {
+  lat: 37.5665,
+  lng: 126.978,
+};
 
 const LocationPage = () => {
+  const [center, setCenter] = useState(defaultCenter);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.error("위치 가져오기 실패:", error);
+      }
+    );
+  }, []);
+
   return (
-    <div>
+    <L.Container>
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
+          <Marker position={center} icon={myPin} />
+        </GoogleMap>
+      </LoadScript>
       <TabBar type="songil" index={0} />
-    </div>
+    </L.Container>
   );
 };
 
